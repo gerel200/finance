b83d15893786b66b8c7c6f879a3142efb6b47b8e
gerel <gerelgerelhen@gmail.com>
2025-08-19 11:56:55 +0900

    delete and update balance sheet

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var uiController = (function () {
     inputDescription: ".add__description",
     inputValue: ".add__value",
     addBtn: ".add__btn",
+    incomeList: ".income__list",
+    expenseList: ".expenses__list",
+    budgetLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expenseLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage",
+    containerDiv: ".container",
   };
 
   return {
@@ -12,29 +19,64 @@ var uiController = (function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value,
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value,
+        value: parseInt(document.querySelector(DOMstrings.inputValue).value),
       };
     },
     getDOMstrings: function () {
       return DOMstrings;
     },
-    addListItem: function(item, type){
-      // inc exp element in html prepare
+
+    clearFields: function () {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputDescription + ", " + DOMstrings.inputValue
+      );
+      // convert list to array
+      var fieldsArr = Array.prototype.slice.call(fields); //for(var i=0; i<fieldsArr.length; i++){fieldsArr[i].value="";} ingj bichij bas boloh bsan
+      fieldsArr.forEach(function (el, index, array) {
+        el.value = "";
+      });
+
+      fieldsArr[0].focus();
+    },
+
+    tosowiigUzuuleh: function (tusuw) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = tusuw.tosow;
+      document.querySelector(DOMstrings.incomeLabel).textContent =
+        tusuw.totalInc;
+      document.querySelector(DOMstrings.expenseLabel).textContent =
+        tusuw.totalExp;
+      if (tusuw !== 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuw.huwi + "%";
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuw.huwi;
+      }
+    },
+
+    deleteListItem: function(id){
+      var el = document.getElementById(id);
+      el.parentNode.removeChild(el);
+    },
+    addListItem: function (item, type) {
+      // to prepare inc exp element in html
       var html, list;
-      if (type === 'inc'){
-        list = '.income__list';
-        html = ' <div class="item clearfix" id="income-%id%"><div class="item__description">%%DESCRIPTION%%</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
-      }else{
-        list = 'expenses__title';
-        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%%DESCRIPTION%%</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
-      };
+      if (type === "inc") {
+        list = DOMstrings.incomeList;
+        html =
+          ' <div class="item clearfix" id="inc-%id%"><div class="item__description">%%DESCRIPTION%%</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else {
+        list = DOMstrings.expenseList;
+        html =
+          '<div class="item clearfix" id="exp-%id%"><div class="item__description">%%DESCRIPTION%%</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
       //in html use REPLACE to change
-      html = html.replace('%id%', item.id);
-      html = html.replace('%%DESCRIPTION%%', item.description);
-      html = html.replace('$$VALUE$$', item.value);
-     //finished html to DOM
-      document.querySelector(list).insertAdjacentHTML('beforeend', html);
-    }
+      html = html.replace("%id%", item.id);
+      html = html.replace("%%DESCRIPTION%%", item.description);
+      html = html.replace("$$VALUE$$", item.value);
+      //finished html to DOM
+      document.querySelector(list).insertAdjacentHTML("beforeend", html);
+    },
   };
 })();
 
@@ -52,51 +94,102 @@ var financeController = (function () {
     this.description = description;
     this.value = value;
   };
+
+  var calculateTotal = function (type) {
+    var sum = 0;
+    data.items[type].forEach(function (el) {
+      sum = sum + el.value;
+    });
+    data.totals[type] = sum;
+  };
   //private data
   var data = {
     items: {
       inc: [],
-      exp: []
+      exp: [],
     },
     totals: {
       inc: 0,
-      exp: 0
-    }
+      exp: 0,
+    },
+
+    tosow: 0,
   };
 
-  return{
-    addItem: function(type, desc, val){
+  return {
+    tusuwTootsooloh: function () {
+      calculateTotal("inc");
+      calculateTotal("exp");
 
+      data.tosow = data.totals.inc - data.totals.exp;
+
+      data.huwi = Math.round((data.totals.exp / data.totals.inc) * 100);
+    },
+
+    tosowiigAwah: function () {
+      return {
+        tosow: data.tosow,
+        huwi: data.huwi,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+      };
+    },
+
+    deleteItem: function (type, id) {
+      var ids = data.items[type].map(function (el) {
+        return el.id;
+      });
+
+      var index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.items[type].splice(index, 1);
+      }
+    },
+
+    addItem: function (type, desc, val) {
       var item, id;
 
-      if (data.items[type].length === 0) id =1;
+      if (data.items[type].length === 0) id = 1;
       else {
         id = data.items[type][data.items[type].length - 1].id + 1;
-      };
+      }
 
-      if(type === 'inc'){
-          item = new Income(id, desc, val);
-      }else {
-          item = new Expense(id, desc, val);
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
       }
 
       data.items[type].push(item);
       return item;
-    }
-  }
+    },
+  };
 })();
 
 // programm connection controller
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
     // 1. oruulah ogogdoliig delgetsnees olj awah
-    console.log(uiController.getInput());
-    // 2. olj awsan ogogdluudee sanhuugiin controller d damjuulj tend hadgalna
-    var item = financeController.addItem(input.type, input.description, input.value);
-    // 3. olj awsan ogogluudee web deeree tohiroh hesegt n gargana
-    uiController.addListItem(item, input.type);
-    // 4. toswiig tootsooloh
-    // 5. etssiin uldegdel tootsoog delgetsend gargana
+    var input = uiController.getInput();
+
+    if (input.description !== "" && input.value !== "") {
+      // 2. olj awsan ogogdluudee sanhuugiin controller d damjuulj tend hadgalna
+      var item = financeController.addItem(
+        input.type,
+        input.description,
+        input.value
+      );
+      // 3. olj awsan ogogluudee web deeree tohiroh hesegt n gargana
+      uiController.addListItem(item, input.type);
+      uiController.clearFields();
+      // 4. toswiig tootsooloh
+      financeController.tusuwTootsooloh();
+      // 5. etssiin uldegdel
+      var tosow = financeController.tosowiigAwah();
+      // 6.  tootsoog delgetsend gargana
+      uiController.tosowiigUzuuleh(tosow);
+    }
   };
 
   var setUpEventListeners = function () {
@@ -110,10 +203,35 @@ var appController = (function (uiController, financeController) {
         ctrlAddItem();
       }
     });
+
+    document
+      .querySelector(DOM.containerDiv)
+      .addEventListener("click", function (event) {
+        console.log(
+          event.target.parentNode.parentNode.parentNode.parentNode.id
+        );
+        if (id) {
+          var arr = id.split("-");
+          var type = arr[0];
+          var itemId = parseInt(arr[1]);
+
+          //1. delete and use type and id
+          financeController.deleteItem(type, itemId);
+          //2. delete element on display
+          uiController.deleteListItem(id);
+          //3. update balance sheet
+        }
+      });
   };
   return {
     init: function () {
       console.log("Application started...");
+      uiController.tosowiigUzuuleh({
+        tosow: 0,
+        huwi: 0,
+        totalInc: 0,
+        totalExp: 0,
+      });
       setUpEventListeners();
     },
   };
